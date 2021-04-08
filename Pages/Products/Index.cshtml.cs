46a4db01dba1a430b176825c4d03345a3f5ddad0
Christopher <drakochris1992@gmail.com>
2021-04-08 14:23:28 -0400

    added prodects generator code

---
+++ Pages/Products/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GscStore.Models;
+
+namespace GscStore.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GscStoreContext _context;
+
+        public IndexModel(GscStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Product.ToListAsync();
+        }
+    }
+}
