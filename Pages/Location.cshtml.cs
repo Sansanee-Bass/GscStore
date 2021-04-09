fcc342369c791c84a5c81f87028fef7f22225d9b
sansanee <sansaneea@gmail.com>
2021-04-09 00:52:37 -0400

    added HomeDesing

--- Pages/Location.cshtml.cs
+++ Pages/Location.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+
 namespace GscStore.Pages
 {
     public class LocationModel : PageModel
